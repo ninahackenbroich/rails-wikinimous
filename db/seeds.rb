72afa7620d8da1ee78b772d64acc70d2e6563ee3
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-11 16:36:29 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+  article_new = Article.new({title: Faker::TvShows::HowIMetYourMother.catch_phrase, content: Faker::TvShows::HowIMetYourMother.quote})
+  article_new.save
+end
